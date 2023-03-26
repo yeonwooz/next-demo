efa0a038047656c5e3b07cfceb2ba7f7d52076fe
suyeon woo <woosean999@gmail.com>
2023-03-26 22:11:57 +0900

    build. 웹팩 설정 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  output: 'standalone',
-}
+  output: "standalone",
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.js/,
+      use: [options.defaultLoaders.babel],
+    });
+    return config;
+  },
+};
